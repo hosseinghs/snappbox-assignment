164b650baa07739eb5dd1714e40d716f60a9960e
hosseinghs <job.hghs@gmail.com>
2024-12-29 16:45:39 +0330

    feat: add timer logic

--- frontend/src/components/base/CodeInput.tsx
+++ frontend/src/components/base/CodeInput.tsx
@@ -1,12 +1,16 @@
 "use client"
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
 
 interface CodeInputComponentProps {
-  length?: number; // Default length of the code inputs is 5
+  length?: number;
+  onResendClick: () => void
 };
 
-const CodeInputComponent: React.FC<CodeInputComponentProps> = ({ length = 5 }) => {
+const CodeInputComponent: React.FC<CodeInputComponentProps> = ({ length = 5, onResendClick }) => {
   const inputsRef = useRef<Array<HTMLInputElement | null>>([]);
+  const [timer, setTimer] = useState(3);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const value = e.target.value;
@@ -20,7 +24,9 @@ const CodeInputComponent: React.FC<CodeInputComponentProps> = ({ length = 5 }) =
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-    if (e.key === 'Backspace' && index > 0 && !inputsRef.current[index]?.value) inputsRef.current[index - 1]?.focus();
+    if (e.key === 'Backspace' && index > 0 && !inputsRef.current[index]?.value) {
+      inputsRef.current[index - 1]?.focus();
+    }
   };
 
   const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
@@ -38,20 +44,55 @@ const CodeInputComponent: React.FC<CodeInputComponentProps> = ({ length = 5 }) =
     e.preventDefault();
   };
 
+  useEffect(() => {
+    if (timer === 0) {
+      setIsDisabled(true);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      if (timer > 0) setTimer((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timer]);
+
+  const handleResetTimer = () => {
+    setTimer(3); 
+    setIsDisabled(false);
+    if (onResendClick) onResendClick()
+  };
+
   return (
-    <div style={{ display: 'flex', gap: '8px' }}>
-      {Array.from({ length }, (_, index) => (
-        <input
-          key={index}
-          type="text"
-          maxLength={1}
-          style={{ width: '40px', height: '40px', textAlign: 'center', fontSize: '20px' }}
-          onChange={(e) => handleInputChange(e, index)}
-          onKeyDown={(e) => handleKeyDown(e, index)}
-          onPaste={handlePaste}
-          ref={(el) => (inputsRef.current[index] = el)}
-        />
-      ))}
+    <div>
+      <div style={{ display: 'flex', gap: '8px' }}>
+        {Array.from({ length }, (_, index) => (
+          <input
+            key={index}
+            ref={(el) => (inputsRef.current[index] = el)}
+            type="text"
+            maxLength={1}
+            style={{ width: '40px', height: '40px', textAlign: 'center', fontSize: '20px' }}
+            disabled={isDisabled}
+            onPaste={handlePaste}
+            onChange={(e) => handleInputChange(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+          />
+        ))}
+      </div>
+
+      <div>
+        {timer > 0 ? (
+          <p>Time remaining: {Math.floor(timer / 60)}:{String(timer % 60).padStart(2, '0')}</p>
+        ) : (
+          <p>New OTP?</p>
+        )}
+      </div>
+
+      {timer === 0 && (
+        <Button onClick={handleResetTimer}>Request New OTP</Button>
+      )}
+
     </div>
   );
 };
