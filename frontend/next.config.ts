3609148d8f9450527fe867cb478d7d7663862b05
hosseinghs <job.hghs@gmail.com>
2024-12-29 18:10:06 +0330

    feat: proxy address

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* use proxy to bypass CORS */ 
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:3000/*',
+        destination: 'http://127.0.0.1:3000/:path*', // Correct the destination path
       },
     ];
   },
