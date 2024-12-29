ad51a76c10bd1544a4cdb3c9b691d3426d97270b
hosseinghs <job.hghs@gmail.com>
2024-12-29 20:30:22 +0330

    remove log

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,8 +2,6 @@ import { getAccessToken } from '@/auth';
 import { NextResponse, NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  console.log('sadasd');
-    
   const token = getAccessToken()
   
   if (!token) return NextResponse.redirect(new URL('/auth/register', request.url))
