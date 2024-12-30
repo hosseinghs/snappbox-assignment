f1595247367394811b3f2b1ea2a9a42dbe9b0a48
hosseinghs <job.hghs@gmail.com>
2024-12-30 15:29:21 +0330

    feat: add commissions page

--- frontend/src/app/dashboard/commission/page.tsx
+++ frontend/src/app/dashboard/commission/page.tsx
@@ -1,9 +1,31 @@
 'use client'
+import { useState, useEffect } from 'react';
+
+import BaseTable from '@/components/base/table'
+import { getAllCommissionsAPI } from '@/services/comissions';
 
 export default function CommissionPage() {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [comissions, setCommissions] = useState([])
+
+  const handleGetCommissionsList = async () => {
+    try {
+      setLoading(true)
+      const { data } = await getAllCommissionsAPI()
+      setCommissions(data)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    handleGetCommissionsList()
+  }, [])
+
+
   return (
     <div style={ { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' } }>
-        test
+        <BaseTable loading={loading} rows={comissions} />
     </div>
   );
 }
