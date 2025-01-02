41965fe1289ce82a90a787f71c4accaa3fee2c4f
hosseinghs <job.hghs@gmail.com>
2025-01-03 00:32:15 +0330

    style

--- frontend/src/components/commission/CommissionInput.tsx
+++ frontend/src/components/commission/CommissionInput.tsx
@@ -1,5 +1,3 @@
-import { TextField } from "@mui/material";
-
 interface IProps {
   value: string;
   onValueChange: (value: string) => void;
@@ -7,9 +5,9 @@ interface IProps {
 
 export default function CommissionInput ({ value, onValueChange }: IProps) {
   return (
-    <div style={ { display: 'flex', alignItems: 'center', borderRadius: '6px', border: '1px solid #D3D8DD', maxWidth: '80px' } }>
-        <TextField value={value} onChange={(e) => onValueChange(e.target.value)} size="small" />
-        <div style={ { backgroundColor: '#F5F5F5', padding: '0 4px' } }>%</div>
+    <div className="flex items-center justify-between max-w-16 h-10 border-[1px] border-gray-300 rounded-md">
+        <input className="pl-2 max-w-10 outline-none bg-white" value={value} onChange={(e) => onValueChange(e.target.value)} type="text" />
+        <div className="h-full flex items-center bg-[#F5F5F5]">%</div>
     </div>
   );
 };
