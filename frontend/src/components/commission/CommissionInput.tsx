15984d855b76f0e4639d3c30bde51636e88b3889
hosseinghs <job.hghs@gmail.com>
2025-01-04 12:22:33 +0330

    style

--- frontend/src/components/commission/CommissionInput.tsx
+++ frontend/src/components/commission/CommissionInput.tsx
@@ -6,15 +6,15 @@ interface IProps {
 
 export default function CommissionInput({ isEdit, value, onValueChange }: IProps) {
   return isEdit ? (
-    <div className="flex items-center justify-between max-w-16 h-10 border-[1px] border-gray-300 rounded-md">
-      <input
-        value={value}
-        type="text"
-        className="pl-2 max-w-10 outline-none bg-white"
-        onChange={(e) => onValueChange(e.target.value)}
-      />
-      <div className="h-full flex items-center bg-[#F5F5F5]">%</div>
-    </div>
+      <div className="flex items-center justify-between h-10 border-[1px] mx-auto border-gray-300 rounded-md">
+        <input
+            value={value}
+            type="text"
+            className="pl-2 outline-none bg-white"
+            onChange={(e) => onValueChange(e.target.value)}
+            />
+          <div className="min-w-8 h-full content-center bg-[#F5F5F5]">%</div>
+        </div>
   ) : (
     <div>{value}%</div>
   );
